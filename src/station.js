29f8de2c3bd2a6324d18e98513497494dfcce58b
Quertte <a.injen90@gmail.com>
2022-08-15 19:15:34 +0300

    classes are release

--- src/station.js
+++ src/station.js
@@ -4,6 +4,29 @@ class Station {
     this.location = location;
     this.bikes = bikes;
   }
+
+  newestBike() {
+    return this.bikes.sort((el1, el2) => el2.modelYear - el1.modelYear)[0];
+  }
+
+  empty() {
+    return this.bikes.length === 0;
+  }
+
+  release() {
+    if (this.empty()) {
+      return undefined;
+    }
+    return this.bikes.shift();
+  }
+
+  take(arg) {
+    this.bikes.push(arg);
+  }
+
+  takeMultiple(arr) {
+    arr.forEach(el => this.bikes.push(el));
+  }
 }
 
 module.exports = Station;
